54049e2857df45969c03e433107276ae32b02820
TÖRÖK Attila <torokati44@gmail.com>
2025-05-07 14:58:29 +0200

    AI(untested): Add support for logging TLS secrets into pcapng files

--- src/inet/common/packet/recorder/PcapRecorder.h
+++ src/inet/common/packet/recorder/PcapRecorder.h
@@ -51,6 +51,10 @@ class INET_API PcapRecorder : public cSimpleModule, protected cListener, public 
     std::vector<IHelper *> helpers;
     PacketPrinter packetPrinter;
 
+    // TLS Secrets Recording
+    bool recordTlsSecrets = false;
+    simsignal_t tlsSecretsSignal;
+
     static simsignal_t packetRecordedSignal;
 
     b frontOffset;
@@ -75,6 +79,7 @@ class INET_API PcapRecorder : public cSimpleModule, protected cListener, public 
     virtual void handleMessage(cMessage *msg) override;
     virtual void refreshDisplay() const override;
     virtual void finish() override;
+    virtual void receiveSignal(cComponent *source, simsignal_t signalID, const char *s, cObject *details) override;
     virtual void receiveSignal(cComponent *source, simsignal_t signalID, cObject *obj, cObject *details) override;
     virtual void recordPacket(const cPacket *msg, Direction direction, cComponent *source);
     virtual bool matchesLinkType(PcapLinkType pcapLinkType, const Protocol *protocol) const;
