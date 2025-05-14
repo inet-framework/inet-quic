3aac65ec7acf1e9265fa4070ad2ba263e9ea935d
TÖRÖK Attila <torokati44@gmail.com>
2025-05-14 18:41:00 +0200

    handle byteschunk in crypto frames via ptls

--- src/inet/transportlayer/quic/connectionstate/InitialConnectionState.cc
+++ src/inet/transportlayer/quic/connectionstate/InitialConnectionState.cc
@@ -11,6 +11,11 @@
 #include "EstablishedConnectionState.h"
 #include "../packet/ConnectionId.h"
 
+extern "C" {
+#include "picotls.h"
+#include "picotls/openssl_opp.h"
+}
+
 namespace inet {
 namespace quic {
 
@@ -26,9 +31,25 @@ ConnectionState *InitialConnectionState::processConnectAppCommand(cMessage *msg)
 void InitialConnectionState::processCryptoFrame(const Ptr<const CryptoFrameHeader>& frameHeader, Packet *pkt)
 {
     if (frameHeader->getContainsTransportParameters()) {
-        auto transportParametersExt = staticPtrCast<const TransportParametersExtension>(pkt->popAtFront());
-        EV_DEBUG << "got transport parameters: " << transportParametersExt << endl;
-        context->getRemoteTransportParameters()->readExtension(transportParametersExt);
+        Ptr<const Chunk> payload = pkt->popAtFront();
+        if (auto transportParametersExt = dynamicPtrCast<const TransportParametersExtension>(payload)) {
+            EV_DEBUG << "got transport parameters: " << transportParametersExt << endl;
+            context->getRemoteTransportParameters()->readExtension(transportParametersExt);
+        }
+        if (auto tlsPayload = dynamicPtrCast<const BytesChunk>(payload)) {
+            EV_DEBUG << "got transport parameter bytes" << endl;
+            ptls_buffer_t buffer;
+            ptls_buffer_init(&buffer, (void*)"", 0);
+            size_t epoch_offsets[5] = {0};
+            std::vector<uint8_t> tlsBytes = tlsPayload->getBytes();
+            ptls_handle_message(context->tls, &buffer, epoch_offsets, 0,
+                tlsBytes.data(), tlsBytes.size(), nullptr);
+            for (int i = 0; i < 5; i++) {
+                if (epoch_offsets[i] > 0) {
+                    std::cout << "epoch " << i << " offset: " << epoch_offsets[i] << endl;
+                }
+            }
+        }
     }
 }
 
